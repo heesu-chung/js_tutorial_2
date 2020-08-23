54535abd3987d966f5a2ca51ad4ed1ffd348efff
heesu-chung <chout1213@gmail.com>
2020-08-23 19:53:54 +0900

    0823_velopert_function

---
+++ function.js
@@ -0,0 +1,26 @@
+/*let a = 5;
+let b = 7;
+function sum(a, b) {
+    let sum;
+    return sum = a + b;
+}
+console.log(sum(a,b));
+*/
+/*
+function add(a,b){
+    return a + b;
+}
+
+
+let a = 2, b=5;
+
+const sum = add(a, b);
+console.log(sum);*/
+
+function hello(name){
+    console.log('hello ' + name + '!');
+}
+
+let name = 'hee su';
+const result = hello(name);
+console.log(result);
